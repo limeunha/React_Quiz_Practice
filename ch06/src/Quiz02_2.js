bfac694282de693ca8b4d7a311905cd22d36cd9f
limeunha <eh1501@naver.com>
2024-11-01 17:44:35 +0900

    수정작업

--- ch06/src/Quiz02_2.js
+++ ch06/src/Quiz02_2.js
@@ -1,13 +1,25 @@
 import React, { useMemo } from 'react'
+import { useState } from 'react'
 
 const Quiz02_1 = ({ numbers }) => {
    // filter, reduce 사용하여 홀수만 필터링하고 그 합계를 계산한 후 useMemo로 처리합니다.
-   //const oddSum =
+   // const oddSum =
+   if (number % 3 === 0) return 0
+   const oddSum = numbers.reduce((a, b) => a + b)
+
+   const useMemo = () => {
+      const [number, setNumber] = useState('')
+
+      const onInset = (e) => {
+         const oddSum = oddSum.concat(number(number))
+         setNumber('')
+      }
+   }
 
    return (
       <div>
          <p>주어진 숫자 배열: {numbers.join(', ')}</p>
-         <p>홀수의 합계: {/* 여기에 결과를 출력하세요 */}</p>
+         <p>홀수의 합계: {/* 여기에 결과를 출력하세요 */ oddSum}</p>
       </div>
    )
 }
