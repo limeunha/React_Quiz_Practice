c8ce95ad0ea81b11d7aa1102138c2ae6f934df44
limeunha <eh1501@naver.com>
2024-11-06 17:42:09 +0900

    수정작업

--- ch11/src/Quiz02/counterReducer.js
+++ ch11/src/Quiz02/counterReducer.js
@@ -4,8 +4,10 @@ function counterReducer(state = initialState, action) {
    switch (action.type) {
       case 'increment':
       // 여기에 코드 작성(숫자를 감소 시킴)
+
       case 'decrement':
       // 여기에 코드 작성(숫자를 증가 시킴)
+
       default:
          return state
    }
